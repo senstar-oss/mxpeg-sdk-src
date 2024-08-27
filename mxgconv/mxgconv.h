a9514f8486003177119f28110316e3fd9a100d35
Mastafa Awal <mastafa.awal@senstar.com>
2024-08-27 13:08:36 -0400

    Update mxgconv to support x64, improve the header

--- mxgconv/mxgconv.h
+++ mxgconv/mxgconv.h
@@ -1,8 +1,8 @@
+#pragma once
+extern "C"
+{
+    __declspec(dllexport) void *InitMxpeg();
 
-__declspec(dllexport) void* InitMxpeg() ;
-
-__declspec(dllexport) void DecodeMxpeg(void * pObject,BYTE * pIn,int len,BYTE * pOut,int * outlen);
-
-__declspec(dllexport) void UnInitMxpeg(void * pObject);
-
-
+    __declspec(dllexport) void DecodeMxpeg(void *pObject, BYTE *pIn, int len, BYTE *pOut, int *outlen);
+    __declspec(dllexport) void UnInitMxpeg(void *pObject);
+}
