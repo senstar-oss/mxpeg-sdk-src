fc8347351a9b6588e5a9c58fecc4dcfd57fda700
Mastafa Awal <mastafa.awal@senstar.com>
2024-09-05 13:56:00 -0400

    stray whitespace

--- mxpeg-decoder/mxpeg-decoder.h
+++ mxpeg-decoder/mxpeg-decoder.h
@@ -7,7 +7,6 @@ extern "C"
     };
 
     __declspec(dllexport) void *mxpeg_decoder_init();
-
     __declspec(dllexport) DecodedFrameInfo mxpeg_decoder_decode(void *pObject, unsigned char *pIn, int len, unsigned char *pOut, int *outlen);
     __declspec(dllexport) void mxpeg_decoder_deinit(void *pObject);
 }
